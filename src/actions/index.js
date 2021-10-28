ad5fddd5671589b063e60314f9719094db45b5ed
Miguel Porras <porraangel@gmail.com>
2021-10-28 00:08:11 -0500

    acrando action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+// require('dotenv').config();
+
+// const { ENV, PORT } = process.env;
 
 export const SaveSearch = payload => ({
     type: 'Save_Search',
@@ -6,40 +9,73 @@ export const SaveSearch = payload => ({
   });
 
 
-export const Search01 = (props) => {
-    console.log('estoy action')
-    console.log(props);
-   
+export const SearchProduct = (props) => {  
   return (dispatch) => {
-  
+     var url = '/';
+      var urlAdmin = '/admin';
+       var GetUrl = window.location.pathname;
+       
+       if(GetUrl != url ){
+         if(GetUrl == urlAdmin)
+         {
+
+         }else{
+          window.location.href = 'http://localhost:8080/'
+         }
+        
+       }
+        console.log(props.search)
     axios({
-      url: `http://localhost:3002/api/search/${props.search}`,
+    
+      url: `${process.env.REACT_APP_API_URL}/api/search/${props.search}`,
       method: 'get',
     })
       .then(({ data }) => {
-        console.log(data)
-        console.log(data.data)
-        
         dispatch(SaveSearch(data.data));
+     
       })
       .catch((err) => dispatch(setError(err)));
   };
 };
 
-export const handleSubmit2 = (props) => {
-  console.log('estoy aqui');
-  console.log(props);
+export const CreateProducts = (props) => {
+
   return (dispatch) => {
     axios({
-      url: `https://localhost:44389/api/business/${props.identification_number}`,
-      method: 'put',
+      url: `${process.env.API_URL}/api/products`,
+      method: 'post',
       data: props,
 
     })
       .then(({ data }) => {
-        alert('su informacion fue guardad');
-        window.location.href = 'http://localhost:8080';
+        alert('Producto Guardado');
+        window.location.href = 'http://localhost:8080/admin';
       })
-      .catch((err) => dispatch(setError(err)));
+     .catch((err) => dispatch(setError(err)));
+  };
+};
+
+
+
+export const  EditProduct = (props) => {
+  var product = {
+       title :  props.title,
+        description : props.description,
+        price : props.price,
+       ulrpicture : props.ulrpicture
+      
+  }
+  return (dispatch) => {
+    axios({
+      url: `${process.env.API_URL}/api/products/${props._id}`,
+      method: 'put',
+      data: product,
+
+    })
+      .then(({ data }) => {
+        alert('Producto Editado Exitosamente');
+         window.location.href = 'http://localhost:8080/admin';
+      })
+     .catch((err) => dispatch(setError(err)));
   };
 };
